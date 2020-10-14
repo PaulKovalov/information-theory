fb57e769f9444f80753d4129f0da105b94c00b50
paul <soapmactavish799@gmail.com>
2020-10-15 00:06:57 +0300

    Fix bug in previous version of encoding, sort out converting bits to codes

--- lzw.cpp
+++ lzw.cpp
@@ -6,11 +6,14 @@
 #include <string>
 #include <unordered_map>
 #include <vector>
+#include <unordered_set>
 
 using namespace std;
 
 int NUM_OF_CHARS = 128;  // consider all ASCII characters
 int powers_of_two[] = {1, 2, 4, 8, 16, 32, 64, 128};
+int MODE_ENCODE = 0;
+int MODE_DECODE = 1;
 
 /**
  * Separate table class to encapsulate implementation.
@@ -19,7 +22,7 @@ class Table {
     struct Trie {
         // better to use unique_ptr, but performance may degrade
         // TODO research this
-        unordered_map<char, Trie*> data;
+        unordered_map<int, Trie*> data;
         int v;
         Trie(int v = -1) : v(v){};
     };
@@ -28,11 +31,20 @@ class Table {
     int CHAR_MIN = static_cast<int>(numeric_limits<char>::min());
 
    public:
-    Table() {
+   /**
+    * Constructs initial table. Depending on mode, maps chars to numbers or vice versa
+    * */
+    Table(int mode) {
         root = new Trie();
-        // generate all possible ascii chars - initial alphabet used for encoding & decoding
-        for (int i = 0; i < NUM_OF_CHARS; ++i) {
-            root->data[char(i)] = new Trie(size++);
+        if (mode == MODE_ENCODE) {
+            // generate all possible ascii chars - initial alphabet used for encoding & decoding
+            for (int i = 0; i < NUM_OF_CHARS; ++i) {
+                root->data[i] = new Trie(size++);
+            }
+        } else if (mode == MODE_DECODE) {
+            for (int i = 0; i < NUM_OF_CHARS; ++i) {
+                // todo
+            }
         }
     }
 
@@ -41,12 +53,12 @@ class Table {
      * returns its value. Otherwise inserts the word and returns the value of the
      * existing prefix of the word and the length of the prefix that exists
      **/
-    pair<int, int> get_prefix_and_insert(char* file_data, int start, int end) {
+    pair<int, int> get_prefix_and_insert(char* data, int start, int end) {
         Trie* t_root = root;
         pair<int, int> ans;
         int common_prefix_length = 0;  // the length of the existing prefix of the word stored in the trie
         for (int i = start; i < end; ++i) {
-            char c = file_data[i];
+            char c = data[i];
             if (t_root->data[c]) {
                 t_root = t_root->data[c];
                 ++common_prefix_length;
@@ -58,16 +70,45 @@ class Table {
         }
         return {t_root->v, common_prefix_length};
     }
+
+    int get_size() {
+        return size;
+    }
 };
 
-char bits_to_char(vector<char>& bits, int start) {
-    char r = 0;
-    int to = min((int)bits.size(), start + 8);
+int bits_to_int(vector<char>& bits, int start, int bits_per_number) {
+    int r = 0;
+    int to = min((int)bits.size(), start + bits_per_number);
     for (int i = start; i < to; ++i) {
         r += (bits[i] == '1' ? powers_of_two[i - start] : 0);
     }
     return r;
 }
+
+vector<char> char_to_bits(char& c) {
+    int r = c;
+    if (r < 0) {
+        r += 256;
+    }
+    // char encodes 8 bits
+    vector<char> bits(8);
+    for (int i = 0; i < 8; ++i) {
+        bits[i] = ('0' + (r % 2));
+        r /= 2;
+    }
+    return bits;
+}
+
+bool is_power_of_two(int n) {
+    if (n == 0)
+        return false;
+    if (n == 1)
+        return true;
+    if (n % 2 == 0)
+        return is_power_of_two(n / 2);
+    return false;
+}
+
 
 int main(int argc, char* argv[]) {
     if (argc != 3) {
@@ -95,37 +136,65 @@ int main(int argc, char* argv[]) {
         // encode data
         vector<char> bits;
         // create table (prefix tree)
-        Table table;
-        int front = 0;
-        while (front < file_length) {
+        Table table(MODE_ENCODE);
+        int front = 0, bits_per_number = 8, current_max_number = 256;
+        int nums_total = 0;
+        while (front + 1 < file_length) {
             pair<int, int> prefix = table.get_prefix_and_insert(file_data, front, file_length);
+            if (table.get_size() == current_max_number) {
+                bits_per_number++;
+                current_max_number *= 2;
+            }
             // convert numbers to bits
             int n = prefix.first;
+            cout << n << " ";
+            ++nums_total;
             // convert number to bits
             vector<char> n_bits;
             while (n) {
                 n_bits.push_back('0' + (n % 2));
                 n /= 2;
             }
-            // align bits if the number of bits is less then 7
-            for (int i = 0; i < 7 - static_cast<int>(n_bits.size()); ++i) {
-                bits.push_back('0');
-            }
             // copy n_bits to bits
             bits.insert(bits.end(), n_bits.begin(), n_bits.end());
+            // align bits if their number is less then bits_per_number
+            bits.insert(bits.end(), max(0, bits_per_number - static_cast<int>(n_bits.size())), '0');
             // advance front pointer
-            front += prefix.second + 1;
+            front += prefix.second;
         }
+        cout << endl;
         ofstream out("enc.txt");
+        int bits_per_char = 8;
         // TODO research here, maybe need to call tie to improve performance
         // write bits to file
-        for (int i = 0; i < bits.size(); i += 8) {
+        for (int i = 0; i < bits.size(); i += bits_per_char) {
             // convert 8 bits to one char
-            out << bits_to_char(bits, i);
+            out << static_cast<char>(bits_to_int(bits, i, bits_per_char));
         }
-
     } else if (mode == "d") {
-        // decode data
+        vector<char> bits;
+        // convert chars in file to a bit array
+        for (int i = 0; i < file_length; ++i) {
+            vector<char> c_bits = char_to_bits(file_data[i]);
+            bits.insert(bits.end(), c_bits.begin(), c_bits.end());
+        }
+        // convert this to vector of ints
+        int bits_per_number = 8, start = 0, current_max_number = 256;
+        vector<int> codes;
+        while (start < bits.size()) {
+            // read bits_per_number_bits from the stream and convert them to the number
+            if (codes.size() == current_max_number) {
+                // increase bit length
+                bits_per_number++;
+                current_max_number *= 2;
+            }
+            codes.push_back(bits_to_int(bits, start, bits_per_number));
+            start += bits_per_number;
+        }
+        for (int i = 0; i < codes.size(); ++i) {
+            cout << codes[i] << " ";
+        }
+        cout << endl;
     } else {
         cout << "Mode must be either \"e\" or \"d\"" << endl;
         return 0;
